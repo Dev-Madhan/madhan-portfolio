180f700a7eb7d100974f4daec66525472ae025fa
dev-madhan24@gmail.com <maddyclickz24@gmail.com>
2025-04-27 21:52:46 +0530

    Projects Section animations done!

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,67 +1,84 @@
-import {FC} from "react";
+import { FC } from "react";
 import Button from "@/src/components/Button";
 
 const navItems = [
     {
-        href: '#',
-        label: 'Home',
+        href: "#",
+        label: "Home",
     },
     {
-        href: '#',
-        label: 'Projects',
+        href: "#",
+        label: "Projects",
     },
     {
-        href: '#',
-        label: 'Testimonials',
+        href: "#",
+        label: "Testimonials",
     },
     {
-        href: '#',
-        label: 'Contact',
+        href: "#",
+        label: "Contact",
     },
-]
+];
 
 const Footer: FC = () => {
-    return (<footer className='bg-gray-green-900 text-white' id='contact'>
-        <div className='container'>
-            <div className='section'>
-                <div className='flex items-center gap-3'>
-                    <div className='size-3 rounded-full bg-green-400'></div>
-                    <span className='uppercase'>Get In Touch</span>
+    return (
+        <footer className="bg-gray-green-900 text-white" id="contact">
+            <div className="container">
+                <div className="section">
+                    <div className="flex items-center gap-3">
+                        <div className="size-3 rounded-full bg-green-400"></div>
+                        <span className="uppercase">Get In Touch</span>
+                    </div>
+                    <div className="grid md:grid-cols-3 md:items-center">
+                        <div className="md:col-span-2">
+                            <h2 className="text-4xl mt-8 md:text-7xl lg:text-8xl font-extralight">
+                                Enough talk, Let&apos;s build greatness together.
+                            </h2>
+                            <a href="mailto:devmadhan24@gmail.com"
+                            target='_blank'>
+                                <Button
+                                    variant="secondary"
+                                    className="mt-8"
+                                    iconAfter={
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth="1.5"
+                                            stroke="currentColor"
+                                            className="size-6"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                                            />
+                                        </svg>
+                                    }
+                                >
+                                    devmadhan24@gmail.com
+                                </Button>
+                            </a>
+                        </div>
+                        <div className="">
+                            <nav className="flex flex-col md:items-end gap-8 mt-16 md:mt-0">
+                                {navItems.map(({ href, label }) => (
+                                    <a href={href} key={label}>
+                                        <Button variant="text" className="text-lg">
+                                            {label}
+                                        </Button>
+                                    </a>
+                                ))}
+                            </nav>
+                        </div>
+                    </div>
                 </div>
-                <div className='grid md:grid-cols-3 md:items-center'>
-                  <div className='md:col-span-2'>
-                    <h2 className='text-4xl mt-8 md:text-7xl lg:text-8xl font-extralight'>Enough talk, Let&apos;s build greatness together.</h2>
-                    <Button variant="secondary"
-                            className='mt-8'
-                            iconAfter={
-                              <svg xmlns="http://www.w3.org/2000/svg"
-                                   fill="none"
-                                   viewBox="0 0 24 24"
-                                   strokeWidth="1.5"
-                                   stroke="currentColor"
-                                   className="size-6">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                                />
-                              </svg>
-                            }>devmadhan24@gmail.com</Button>
-                  </div>
-                  <div className=''>
-                    <nav className='flex flex-col md:items-end gap-8 mt-16 md:mt-0'>
-                      {navItems.map(({href, label}) => (
-                          <a href={href} key={label}>
-                            <Button variant="text" className='text-lg'>{label}</Button>
-                          </a>
-                      ))}
-                    </nav>
-                  </div>
-                </div>
+                <p className="py-16 text-white/30 text-sm">
+                    Copyright © Madhan Kumar • All rights reserved
+                </p>
             </div>
-            <p className='py-16 text-white/30 text-sm'>Copyright &copy; Madhan Kumar &bull; All rights reserved</p>
-        </div>
-    </footer>);
+        </footer>
+    );
 };
 
 export default Footer;
