5b331619178c5cc40214fd1d81e67acf37fe50eb
dev-madhan24@gmail.com <maddyclickz24@gmail.com>
2025-05-09 13:52:35 +0530

    Intro section content changed.

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,19 +1,20 @@
 "use client";
 
-import { FC, useEffect, useRef } from "react";
+import {FC, useEffect, useRef} from "react";
 import { useInView } from "framer-motion";
 import useTextRevealAnimation from "@/src/hooks/useTextRevealAnimation";
 
 const Intro: FC = () => {
   const sectionRef = useRef<HTMLElement>(null);
-  const { scope, entranceAnimation } = useTextRevealAnimation();
+  const {scope, entranceAnimation} = useTextRevealAnimation();
+
   const inView = useInView(scope, {
     once: true,
     margin: "0px 0px -50px 0px", // Trigger animation slightly before fully in view
   });
 
   useEffect(() => {
-    if (inView) {
+    if (inView){
       entranceAnimation();
     }
   }, [inView, entranceAnimation]);
@@ -25,12 +26,8 @@ const Intro: FC = () => {
           ref={sectionRef}
       >
         <div className="container">
-          <h2
-              className="text-4xl md:text-7xl lg:text-8xl lg:w-[80%] font-medium"
-              ref={scope}
-          >
-            Designing striking websites with pristine code and insightful aesthetics
-            to boost businesses online.
+          <h2 className="text-4xl md:text-7xl lg:text-8xl lg:w-[80%] font-medium" ref={scope}>
+              Aspiring Front-end Developer creating striking, clean-coded websites to elevate businesses online
           </h2>
         </div>
       </section>
